a8a46f35235f7b6e9938b1aaa9b342a20c720d76
avocado-don <hatonomuneniku79@yahoo.co.jp>
2023-02-15 18:01:49 +0900

    messagesテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,4 @@ class User < ApplicationRecord
   has_many :messages
 
   validates :name, presence: true
-
-  has_many :room_users
-  has_many :rooms, through: :room_users
-  has_many :messages
 end
